c9369479c452e0921c8d281c168ede2ca06b4955
wakan <wakan@LAPTOP-QDFSJ3J6>
2024-11-16 09:47:24 +0530

    commit from keertika

---
+++ Automation_With_java_Selenium_8/src/test/java/keertika/ToLearnGitHub.java
@@ -0,0 +1,11 @@
+package keertika;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ToLearnGitHub {
+@Test 
+public void forkAndPullRequent() {
+	Reporter.log("ForkAndPullRequent send successfully");
+}
+}
