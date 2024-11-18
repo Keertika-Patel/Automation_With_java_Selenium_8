3bb84949eebb52e559d14376daad43987c021387
Mohan <Mohan@LAPTOP-SV3CMC9U>
2024-11-18 09:11:06 +0530

    Added sync fork changes

---
+++ Automation_With_java_Selenium_8/src/test/java/giftcards/SyncForkTest.java
@@ -0,0 +1,13 @@
+package giftcards;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SyncForkTest {
+	
+	@Test
+	public void syncForkExample() {
+		Reporter.log("Testing the sync fork -Update branch", true);
+	}
+
+}
