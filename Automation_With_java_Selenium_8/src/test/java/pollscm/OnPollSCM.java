5374be60380e113374a0f7a0b951bbeb9db03980
wakan <wakan@LAPTOP-QDFSJ3J6>
2024-11-20 09:30:17 +0530

    Schedule poll SCM

---
+++ Automation_With_java_Selenium_8/src/test/java/pollscm/OnPollSCM.java
@@ -0,0 +1,11 @@
+package pollscm;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class OnPollSCM {
+@Test
+public void pollScm() {
+	Reporter.log("Schedule poll SCM");
+}
+}
